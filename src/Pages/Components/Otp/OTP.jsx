5f09d4a61a6394bc6ab8d8169e4302757bde8944
chhotelal Patel <chhotupatel707@gmail.com>
2025-08-23 11:21:56 +0530

    Update

--- src/Pages/Components/Otp/OTP.jsx
+++ src/Pages/Components/Otp/OTP.jsx
@@ -1,40 +1,46 @@
+import { FastField } from "formik";
 import React, { useState } from "react";
 import { OtpInput } from "reactjs-otp-input";
 
 const OTP = () => {
   const [otp, setOtp] = useState("");
-  console.log(otp, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-  const handleChange = (otp) => setOtp(otp);
+  console.log(otp, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <div className="w-full h-[500px] flex justify-center">
+      <div className="w-full flex justify-center">
         <OtpInput
           value={otp}
-          onChange={handleChange}
+          onChange={(otp) => setOtp(otp)}
           numInputs={6}
-          // separator={<span>-</span>}
+          // the below style apply on the whole container (all input boxes)
           containerStyle={{
-            width: "15%",
-            height: "50%",
             display: "flex",
             flexDirection: "row",
             justifyContent: "center",
             alignItems: "center",
             gap: 5,
           }}
+          // the below style apply on each input box
           inputStyle={{
-            padding: "20px",
-            borderRadius: "4px",
+            width: "40px",
+            height: "40px",
+            borderRadius: "6px",
             textAlign: "center",
-            fontSize: "10px",
-            color: "black",
-            border: "1px solid red",
-            width: "10%",
-            height: "100%",
+            fontSize: "18px",
+            color: "#A8A8A8",
+            border: "1px solid gray",
+            outline: "none", // This prevents the default browser outline
           }}
-          isInputNum
+          focusStyle={
+            {
+              // border: "2px solid #A8A8A8",
+            }
+          }
+          // isDisabled={true} // disable all input fields
+          shouldAutoFocus={false}
+          isInputNum={true}
+          // placeholder="otplll"
         />
       </div>
     </div>
